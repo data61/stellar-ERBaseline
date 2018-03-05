41535370a6d10425521b65cbf1f9e592f8c646d6
Jeff Wang <wangzhen263@gmail.com>
2018-03-05 23:30:52 +1100

    FUCK travis does not load s3 maven2

--- configure-maven.sh
+++ configure-maven.sh
@@ -11,15 +11,3 @@ sed -i~ "/<servers>/ a\
   <username>${MAVEN_USERNAME}</username>\
   <password>${MAVEN_PASSWORD}</password>\
 </server>" /usr/share/maven/conf/settings.xml
-
-sed -i~ "/<servers>/ a\
-<server>\
-  <id>Serene-release</id>\
-  <username>${MAVEN_USERNAME}</username>\
-  <password>${MAVEN_PASSWORD}</password>\
-</server>\
-<server>\
-  <id>Serene-snapshot</id>\
-  <username>${MAVEN_USERNAME}</username>\
-  <password>${MAVEN_PASSWORD}</password>\
-</server>" $HOME/.m2/settings.xml
